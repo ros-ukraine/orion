9fbb1337bb66f279881dfa93823bea08149c1852
Bogdan <xeedjr@gmail.com>
2020-09-01 11:56:01 +0300

    add minor draft

--- orion_protocol/include/orion_protocol/orion_minor.h
+++ orion_protocol/include/orion_protocol/orion_minor.h
@@ -8,14 +8,27 @@
 #ifndef THIRD_PARTY_LIBS_ORION_ORION_MINOR_H_
 #define THIRD_PARTY_LIBS_ORION_ORION_MINOR_H_
 
+#include "orion_protocol/orion_transport.h"
+#include "orion_protocol/orion_header.h"
+#include "orion_protocol/orion_timeout.h"
+
+
 namespace orion {
 
 class Minor {
 public:
-	Minor();
-	virtual ~Minor();
+	explicit Minor(Transport *transport) : transport_(transport) {}
+	virtual ~Minor() {};
 
 	void receiveCommand(void);
+	bool processPacket(const CommandHeader *command_header, Timeout &timeout, size_t &size_received);
+
+	  Transport *transport_;
+
+	  static const uint32_t BUFFER_SIZE = 500;
+	  uint8_t result_buffer_[BUFFER_SIZE];
+
+	  uint32_t sequence_id_ = 0;
 };
 
 } /* namespace orion */
--- orion_protocol/src/orion_minor.cpp
+++ orion_protocol/src/orion_minor.cpp
@@ -6,21 +6,78 @@
  */
 
 #include "orion_protocol/orion_minor.h"
+#include <stdexcept>
 
 namespace orion {
 
-Minor::Minor() {
-	// TODO Auto-generated constructor stub
+#pragma pack(push, 1)
+
+struct HandshakeCommand
+{
+  orion::CommandHeader header =
+  {
+    .frame = { .crc = 0 },
+    .common = { .message_id = 2, .version = 1, .oldest_compatible_version = 1, .sequence_id = 0 }
+  };
+};
+
+struct HandshakeResult
+{
+  orion::ResultHeader header =
+  {
+    .frame = { .crc = 0 },
+    .common = { .message_id = 2, .version = 1, .oldest_compatible_version = 1, .sequence_id = 0 },
+    .error_code = 0
+  };
+};
+
+#pragma pack(pop)
+
+void Minor::receiveCommand(void)
+{
+	HandshakeCommand command;
+	uint8_t retry_count = 2;
+	uint32_t retry_timeout = 500;
+
+    CommandHeader *command_header = reinterpret_cast<CommandHeader*>(&command);
+
+    bool received = false;
+    size_t size_received = 0;
+    while ((retry_count > 0) && (false == received))
+    {
+      Timeout timeout(retry_timeout);
+      command_header->common.sequence_id = ++(this->sequence_id_);
+      {
+    	  	 received = processPacket(command_header, timeout, size_received);
+      }
+      retry_count--;
+    }
+    if (false == received)
+    {
+      throw std::runtime_error("Timeout expired but result was not received");
+    }
 
 }
 
-Minor::~Minor() {
-	// TODO Auto-generated destructor stub
+bool Minor::processPacket(const CommandHeader *command_header, Timeout &timeout, size_t &size_received)
+{
+	  size_received = this->transport_->receivePacket(this->result_buffer_, BUFFER_SIZE, timeout.timeLeft());
+	  do
+	  {
+	    if (size_received >= sizeof(ResultHeader))
+	    {
+	    	command_header = reinterpret_cast<CommandHeader*>(this->result_buffer_);
+	    }
+	    if (this->transport_->hasReceivedPacket() && timeout.hasTime())
+	    {
+	      size_received = this->transport_->receivePacket(this->result_buffer_, BUFFER_SIZE, timeout.timeLeft());
+	    }
+	  }
+	  while (timeout.hasTime());
+
+	  return true;
 }
 
-void Minor::receiveCommand(void) {
 
-
-}
 
 } /* namespace orion */
